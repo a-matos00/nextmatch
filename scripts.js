9bffa267e3ee7d1fe60c5049cab1fa04398f2652
Andrija Matosic <56841259+a-matos00@users.noreply.github.com>
2021-12-13 18:56:34 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,11 @@
 var i = 0;	//data iterator
 var data;	//api data
 var xhr;	//xmlHttp request
-
-getData(12,1);	//store API data locally
-
+const d = new Date();
+let month = d.getMonth();
+let day = d.getDay() + 1;
+document.getElementById("matchup").innerHTML = "Real Madrid" + "<br/>" + "VS" + "<br/>" + "PSG";
+//getData(day, month);	//store API data locally
 
 
 
