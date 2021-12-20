685919166fc02063bd3e0374622787f28646512e
Andrija Matosic <56841259+a-matos00@users.noreply.github.com>
2021-12-20 18:21:33 +0100

    Update function_library.js

--- function_library.js
+++ function_library.js
@@ -6,7 +6,8 @@ function getData(day, month){	//makes the API call and stores the data locally
 
 	xhr.open("GET", "https://football-prediction-api.p.rapidapi.com/api/v2/predictions?federation=UEFA&iso_date=2021-"+day+"-"+month + "&market=classic");
 	xhr.setRequestHeader("x-rapidapi-host", "football-prediction-api.p.rapidapi.com");
-	xhr.setRequestHeader("x-rapidapi-key", "b2bc59d145mshe59398052477024p1bd251jsne9b034b4d937");
+	xhr.setRequestHeader("x-rapidapi-key", "bc90667614msha72a825a2b31b88p131ffbjsnf441aefb0128");
+console.log(day + " " + month);
 
 	xhr.send(data);
 
@@ -17,26 +18,59 @@ function getData(day, month){	//makes the API call and stores the data locally
 
         	console.log(this.response.data[i].home_team); 
 			console.log("Data loaded");
+
+			document.getElementById("matchup").innerHTML = xhr.response.data[i].home_team +"   VS   "+ xhr.response.data[i].away_team + "   Prediction:"+xhr.response.data[i].prediction;
 		}
 	});
 
+/*
+	setTimeout(function(){
+			displayAll();
+
+	}, 2000); 
+*/
 
 	
 }
 
 
 function next(){
+/*
 	document.getElementById("matchup").innerHTML = "Real Madrid" + "<br/>" + "VS" + "<br/>" + "PSG";
-	/*if(i >= xhr.response.data.length ){
-		document.getElementById("matchup").innerHTML = "Real Madrid" + "<br/>" + "VS" + "<br/>" + "PSG";
-		//document.getElementById("root").innerHTML = "No more matches";
+document.getElementById("prediction").innerHTML = "Prediction: X";
+document.getElementById("date").innerHTML = "12.12.2021 21:00";
+*/
+	if(i >= xhr.response.data.length ){
+		document.getElementById("main_box").innerHTML = "No more matches";
 	}
 	else{
-	     document.getElementById("matchup").innerHTML = "Real Madrid" + "<br/>" + "VS" + "<br/>" + "PSG";
-		document.getElementById("matchup").innerHTML = xhr.response.data[i].home_team + " <br/>" +" VS " + "<br/>" + xhr.response.data[i].away_team + "   Prediction:"+xhr.response.data[i].prediction;
-
+			document.getElementById("matchup").innerHTML = xhr.response.data[i].home_team +"   VS   "+ xhr.response.data[i].away_team + "   Prediction:"+xhr.response.data[i].prediction;
+			document.getElementById("matchup").innerHTML = xhr.response.data[i].home_team +"   VS   "+ xhr.response.data[i].away_team + "   Prediction:"+xhr.response.data[i].prediction;
 
 		i++;
 	}
-*/
+
+}
+
+function displayAll()
+{
+	for( i = 0; i < 6; i++)
+	{
+		var newDiv = document.createElement("div");
+		var newName = "matchBox" + i.toString();
+		var className = "matchboxes"
+		newDiv.id = newName;
+		newDiv.class = className;
+		const newContent = document.createTextNode(xhr.response.data[i].home_team + " <br/>" +" VS " + "<br/>" + xhr.response.data[i].away_team + "   Prediction:"+xhr.response.data[i].prediction);
+		newDiv.appendChild(newContent);
+		document.getElementById("prediction").append(newDiv);	//ovo prominit na stvarni element
+	}
+}
+
+function login(){	//povezat na botun
+	username = document.getElementById("input_username").value;
+
+	window.location.href = 'homepage.html';
+	console.log(username);
+	document.getElementById("username").innerHTML = username;
 }
